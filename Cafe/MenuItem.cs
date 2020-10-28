cbff10e3e787722370926b6d422b4fcc5690de2e
JoshHambright <josh.hambright@gmail.com>
2020-10-28 10:16:40 -0400

    Cafe- Menu Class and MenuRepo

---
+++ Cafe/MenuItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe
+{
+    public class MenuItem
+    {
+        // Properties
+        public int MealNumber { get; set; }
+        public string MealName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Ingredients { get; set; }
+        public double Price { get; set; }
+
+
+        public MenuItem() { }
+
+        public MenuItem(int mealNum, string mealName, string desc, string ingredients, double price)
+        {
+            MealNumber = mealNum;
+            MealName = mealName;
+            Description = desc;
+            Ingredients = ingredients;
+            Price = price;
+        }
+
+    }
+}
---
+++ Cafe_Tests/MenuItem_RepoTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Cafe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cafe_Tests
+{
+    [TestClass]
+    public class MenuItem_RepoTests
+    {
+        [TestMethod]
+        public void AddToRepo_ShouldGetCorrectBool() //Create
+        {
+            //Arrange
+            MenuItem item = new MenuItem();
+            MenuItem_Repo menu = new MenuItem_Repo();
+            //Act
+            bool addResult = menu.AddMenuItem(item);
+
+            //Assert
+            Assert.IsTrue(addResult);
+        }
+    }
+}
